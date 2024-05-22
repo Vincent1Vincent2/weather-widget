a87dd610159a4cddd14fb943da50370f8af3d405
Vincent <Vincent.ahlin99@gmail.com>
2024-05-22 18:46:38 +0200

    console log weather data based on location

---
+++ src/app/components/Location.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useLocation = () => {
+  const [location, setLocation] = useState<{ lat: number; lon: number } | null>(
+    null
+  );
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      },
+      (error) => {
+        setError("Unable to retrieve your location");
+      }
+    );
+  }, []);
+
+  return { location, error, setLocation, setError };
+};
+
+export default useLocation;
